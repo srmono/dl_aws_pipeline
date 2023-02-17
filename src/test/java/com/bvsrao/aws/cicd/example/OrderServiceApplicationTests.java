dd21e10ef4364e3ad62d1f1b8f389f78b151c251
srmono <bvsrao91@gmail.com>
2023-02-17 05:37:09 +0530

    spring app updated for aws devops

---
+++ src/test/java/com/bvsrao/aws/cicd/example/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvsrao.aws.cicd.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
